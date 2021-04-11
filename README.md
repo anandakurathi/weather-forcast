e863a1787fbbc431a1c6cf13898612f8992e1a87
Akurathi Anand Kumar <akurathi.kumar@lavu.com>
2021-04-12 00:55:23 +0530

    Base setup of application DB migration file Bootstrap the application with autoload

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # weather-forcast
 weather-forcast
+
+As a User I want a mechanism capable to examine whether data and depending
+on the temperature send a sms message to a specified number.
+
+Repeat the above procedure every 10 minutes for 10 times and then stop.
+
+### The tech stack using
+PHP 7.2
+MySQL 5.6
+
+
+### Installation 
+using Composer for dependency / package manger.
+using "vlucas/phpdotenv" for environment variables handling
+```
+{
+    "require": {
+        "vlucas/phpdotenv": "^2.4"
+    },
+    "autoload": {
+        "psr-4": {
+            "Src\\": "src/"
+        }
+    }
+}
+```
+install our dependencies
+``composer install``
+
+copy `.env.example` to `.env`
+
+### Configure a Database for Your PHP REST API
+```
+mysql -uroot -p
+CREATE DATABASE weather CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER 'weather_user'@'localhost' identified by 'weather_password';
+GRANT ALL on weather.* to 'weather_user'@'localhost';
+quit
+```
---
+++ migration.php
@@ -0,0 +1,31 @@
+<?php
+require 'bootstrap.php';
+
+$statement = <<<EOS
+    CREATE TABLE routee_token (
+      id int(11) unsigned NOT NULL AUTO_INCREMENT,
+      token varchar(200) NOT NULL,
+      expiry datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+      updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      PRIMARY KEY (id)
+    ) ENGINE=InnoDB;
+
+    CREATE TABLE jobs (
+      id int(11) unsigned NOT NULL AUTO_INCREMENT,
+      payload text,
+      status enum('NEW','PROCESSING','DONE','ERROR') DEFAULT NULL,
+      whento datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+      updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      PRIMARY KEY (id)
+    ) ENGINE=InnoDB;
+
+EOS;
+
+try {
+    $createTable = $dbConnection->exec($statement);
+    echo "Success!\n";
+} catch (\PDOException $e) {
+    exit($e->getMessage());
+}
